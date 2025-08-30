a3607dc1962d69554e7bda568c4c363928ad2467
Roberto Santizo <soportetecnico.tejar@legumex.net>
2025-08-30 07:43:45 -0600

    bugfix create token

--- src/controllers/TokenController.ts
+++ src/controllers/TokenController.ts
@@ -1,17 +1,23 @@
 import { Request, Response } from "express";
 import { Token } from "../entity/Token";
 import { generateToken } from "../utils/token";
-import AppDatasource from "../datasources/data-source-app-db";
 import { User } from "../entity/User";
 import { checkPassword } from "../utils/auth";
+import AppDatasource from "../datasources/data-source-app-db";
 
 export class TokenController {
     static async createAPIKey(req: Request, res: Response) {
         const { email, password } = req.body;
 
         const userRepository = AppDatasource.getRepository(User);
-        const user = await userRepository.findOneBy({ email: email });
+        const tokenRepository = AppDatasource.getRepository(Token);
 
+        const user = await userRepository.findOneBy({ email: email });
+        const tokenExists = await tokenRepository.findOneBy({user: { id: user.id}});
+
+       if(tokenExists){
+        await tokenRepository.remove(tokenExists);
+       }
 
         if (!user) {
             res.status(404).send({ error: 'Usuario no Encontrado' });
@@ -25,6 +31,7 @@ export class TokenController {
             res.status(401).json({ error: error.message });
             return;
         }
+
 
         const token = new Token();
         token.token = generateToken();
